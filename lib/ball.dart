ada37f529c609b0603458b7d798dcbec45469075
Erik Rosengren <e@roseng.ren>
2024-03-16 14:10:26 +0100

    dont recreate

--- lib/ball.dart
+++ lib/ball.dart
@@ -201,7 +201,6 @@ class Ball extends BodyComponent with ContactCallbacks {
 // update body
     final shape = body.fixtures.first.shape as CircleShape;
     shape.radius = radius;
-    this.body = createBody();
     position.xy = offset;
   }
 }
